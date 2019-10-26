539b9d6cf406ceb768168a7e5ffea1e3e1dce997
Slmnksm <selman.yurtaslan@gmail.com>
2019-10-26 16:05:16 +0300

    bookservice classes modified

--- src/main/java/tr/kasim/bookapi/service/BookService.java
+++ src/main/java/tr/kasim/bookapi/service/BookService.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tr.kasim.bookapi.service;
 
 import java.util.List;
